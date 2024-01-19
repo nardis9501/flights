95ed555c7214a5f420abb8e5e4ff468faf815cb9
Nardis <nardis9501@gmail.com>
2024-01-18 20:46:40 -0500

    feat: add responsive navbar with framer motion

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 export default function Layout(props) {
   const [text, setText] = useState("");
@@ -36,18 +37,22 @@ export default function Layout(props) {
       <div className="absolute overflow-auto conteiner p-4 px-8 top-0 left-0 w-full h-screen bg-gradient-to-r from-sky-700 to-teal-500 dark:bg-gradient-to-r dark:from-sky-950 dark:to-teal-950">
         <div className="flex  items-center place-content-between">
           <Link
-            className="capitalize text-amber-600 text-xl md:text-3xl"
+            className="capitalize font-semibold opacity-90 hover:opacity-100 text-xl md:text-3xl"
             to={"/"}
           >
-            flights
+            <p className="text-slate-300">
+              trip
+              <span className="text-secondary">flights</span>
+            </p>
           </Link>
           <button
             onClick={handlerClick}
-            className="dark:text-slate-300 text-slate-600"
+            className="z-20 dark:text-slate-300 text-slate-600"
           >
             {text}
           </button>
         </div>
+        <Navbar />
         <Outlet />
       </div>
     </>
---
+++ src/components/Navbar/Navbar.data.jsx
@@ -0,0 +1,11 @@
+import { RiHome3Line } from "react-icons/ri";
+import { TbPlaneInflight } from "react-icons/tb";
+
+export const dataNavbar = [
+  { name: "home", path: "/", icon: <RiHome3Line size={30} /> },
+  {
+    name: "flights",
+    path: "/flights",
+    icon: <TbPlaneInflight size={30} />,
+  },
+];
