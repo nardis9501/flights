cd6b18370c5fab16ac4d16280a8b3c1849f3b3fe
Nardis <nardis9501@gmail.com>
2024-01-19 11:06:16 -0500

    fix: change paging logic

--- src/components/Butons/paginationButton/PaginationButton.jsx
+++ src/components/Butons/paginationButton/PaginationButton.jsx
@@ -4,50 +4,60 @@ export default function PaginationButton({
   resources,
   currentPage,
   setCurrentPage,
+  totalPage,
 }) {
   const [disabled, setDisabled] = useState(currentPage <= 1 ? true : false);
   const [disabledButtoPlus, setDisabledButtoPlus] = useState(false);
   console.log(currentPage);
   useEffect(() => {
-    if (resources && resources.length === 0) {
-      return setDisabledButtoPlus(true);
+    if (currentPage >= totalPage) {
+      setDisabledButtoPlus(true);
+    } else {
+      setDisabledButtoPlus(false);
     }
-    setDisabledButtoPlus(false);
-  }, [resources]);
+
+    if (resources && resources.length === 0) {
+      setCurrentPage(totalPage);
+    }
+  }, [totalPage]);
   const handlerClick = (e) => {
     if (e.target.value === "-") {
-      //   setDisabledButtoPlus(false);
+      if (currentPage <= totalPage) {
+        setDisabledButtoPlus(false);
+      }
+      setCurrentPage(currentPage - 1);
 
-      if (currentPage >= 3) {
-        setCurrentPage(currentPage - 1);
-      } else {
-        setCurrentPage(currentPage - 1);
+      if (currentPage < 3) {
         setDisabled(true);
       }
       return setCurrentPage(currentPage - 1);
     }
-    setDisabled(false);
 
-    return setCurrentPage(currentPage + 1);
+    setDisabled(false);
+    setCurrentPage(currentPage + 1);
+    if (currentPage >= totalPage - 1) {
+      return setDisabledButtoPlus(true);
+    }
   };
   return (
     <>
-      <input
-        onClick={handlerClick}
-        disabled={disabledButtoPlus}
-        type="button"
-        value={"+"}
-        className={`${
-          disabledButtoPlus ? "opacity-55" : ""
-        } text-black place-content-center bg-white  m-2 rounded-md w-10 h-10`}
-      />
       <input
         onClick={handlerClick}
         disabled={disabled}
         type="button"
         value={"-"}
         className={`${
-          disabled ? "opacity-55" : ""
+          disabled && "opacity-55"
+        } text-black place-content-center bg-white  m-2 rounded-md w-10 h-10`}
+      />
+      {currentPage}/{totalPage}
+      <input
+        onClick={handlerClick}
+        disabled={disabledButtoPlus}
+        type="button"
+        value={"+"}
+        className={`${
+          disabledButtoPlus && "opacity-55"
         } text-black place-content-center bg-white  m-2 rounded-md w-10 h-10`}
       />
     </>
--- src/pages/Flifhts/FlightsPage.jsx
+++ src/pages/Flifhts/FlightsPage.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
 import Card from "../../components/Card/Card";
 import PaginationButton from "../../components/Butons/paginationButton/PaginationButton";
+import { useParams } from "react-router-dom";
 export default function FlightsPage(props) {
   const [loading, setLoading] = useState(false);
   const [flights, setFlights] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [disabled, setDisabled] = useState(currentPage <= 1 ? true : false);
-  const [disabledButtoPlus, setDisabledButtoPlus] = useState(false);
+  const [totalPage, setTotalPage] = useState(1);
   const [size, setSize] = useState(() => {
     const getSizeFromStorage = window.localStorage.getItem("size");
     {
       return getSizeFromStorage ? getSizeFromStorage : 10;
     }
   });
+  const { pageId } = useParams();
+  console.log(pageId);
 
   useEffect(() => {
     setLoading(true);
@@ -29,6 +31,7 @@ export default function FlightsPage(props) {
       })
       .then((data) => {
         setFlights(data);
+        setTotalPage(Math.ceil(data.total / size));
       })
       .catch((err) => {
         setError(err);
@@ -41,23 +44,7 @@ export default function FlightsPage(props) {
 
   //console.log(flights);
   const { resources } = flights;
-
-  const handlerClick = (e) => {
-    if (e.target.value === "-") {
-      //   setDisabledButtoPlus(false);
-
-      if (currentPage >= 3) {
-        setCurrentPage(currentPage - 1);
-      } else {
-        setCurrentPage(currentPage - 1);
-        setDisabled(true);
-      }
-      return setCurrentPage(currentPage - 1);
-    }
-    setDisabled(false);
-
-    return setCurrentPage(currentPage + 1);
-  };
+  console.log(totalPage);
 
   const handleCallback = (newSize) => {
     console.log(newSize);
@@ -85,6 +72,7 @@ export default function FlightsPage(props) {
             resources={resources}
             currentPage={currentPage}
             setCurrentPage={handleSetCurrentPage}
+            totalPage={totalPage}
           />
         </div>
       </div>
